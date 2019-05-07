2c824d572d809862a9c0aa5e9f931eeb825c8a50
Tay <tayd93@hotmail.com>
2019-05-07 15:58:29 -0600

    Adds empty? method to cell class and cell test

--- lib/cell.rb
+++ lib/cell.rb
@@ -8,37 +8,11 @@ def initialize(coordinate)
 end
 
 def empty?
-  if @ship == nil
-    true
-  else
-    false
-  end
-end
-
-def place_ship(ship)
-  @ship = ship
-end
-
-def fired_upon?
-  @fired_upon
-end
-
-def fire_upon
-  @fired_upon = true
-  if empty? == false
-    @ship.hit
-  end
-end
-
-def render
- if @fired_upon == false
-   @coordinate = "."
- elsif @fired_upon == true && empty? == true
-   @coordinate = "M"
- elsif @fired_upon == true && empty? == false && @ship.sunk? == false
-   @coordinate = "H"
-elsif @fired_upon == true && empty? == false && @ship.sunk? == true
-   @coordinate = "X"
+   if @ship == nil
+     true
+   else
+      false
+   end
  end
-end
+
 end
