21550737bf7cfec21d42d7ee2de2f13445115a1e
Tay <tayd93@hotmail.com>
2019-05-07 15:56:26 -0600

    Adds Initialize method for Cell class

---
+++ lib/cell.rb
@@ -0,0 +1,44 @@
+class Cell
+attr_reader :coordinate, :ship
+
+def initialize(coordinate)
+  @coordinate = coordinate
+  @fired_upon = false
+  @ship = nil
+end
+
+def empty?
+  if @ship == nil
+    true
+  else
+    false
+  end
+end
+
+def place_ship(ship)
+  @ship = ship
+end
+
+def fired_upon?
+  @fired_upon
+end
+
+def fire_upon
+  @fired_upon = true
+  if empty? == false
+    @ship.hit
+  end
+end
+
+def render
+ if @fired_upon == false
+   @coordinate = "."
+ elsif @fired_upon == true && empty? == true
+   @coordinate = "M"
+ elsif @fired_upon == true && empty? == false && @ship.sunk? == false
+   @coordinate = "H"
+elsif @fired_upon == true && empty? == false && @ship.sunk? == true
+   @coordinate = "X"
+ end
+end
+end
