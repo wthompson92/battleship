fc7c406bb881aa7f3d633ae59391562a9cc86311
username <wnt925@gmail.com>
2019-05-07 23:29:24 -0600

    Add valid coordinate and valid placement methods

--- lib/board.rb
+++ lib/board.rb
@@ -1,32 +1,52 @@
+require './lib/ship'
+require './lib/cell'
 class Board
   attr_reader :cells
 
   def initialize
     @size = 16
-    @cells = Hash.new(0)
+    @keys = []
     @row = ("A".."D")
     @column = (1..4)
     @num = 0
+    @cells = {}
+    @coordinate = "A1"
 
   end
 
   def create_board
     @row.map do |letter|
     @column.map do |number|
-      @cells.store((letter + number.to_s), nil)
+      @keys << (letter + number.to_s)
       end
     end
-  end
+
+   @keys.each do |key|
+    @cells[key] = Cell.new(key.to_s)
+    end
+  @cells.keys
+end
 
 
 
 
   def valid_coordinate?(coordinate)
-     @cells.include?(coordinate)
-   end
+   @keys.include?(coordinate)
+end
+
+  def valid_placement(ship, coordinates)
+    coordinates.all? do |coordinate|
+    if   @keys.include?(coordinate.to_s) && coordinates.length == ship.length
+      true
+    end
+  end
+end
 end
 
 
 board = Board.new
-#p @cells.class
-p board.create_board
+cruiser = Ship.new("Cruiser", 3)
+ board.create_board
+ board.valid_coordinate?("A9")
+
+p board.valid_placement(cruiser, ["A1", "A2", "A3"])
