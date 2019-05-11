d0ad2df179481e42190b2ae18d000b86d30f81dc
Tay <tayd93@hotmail.com>
2019-05-11 11:29:10 -0600

    Adds start method

--- lib/message.rb
+++ lib/message.rb
@@ -1,21 +1,22 @@
 class Message
   #attr_reader
 
-def initialize
-end
+  def start_battleships
+    puts "Welcome to BATTLESHIP"
+    puts "Enter p to play. Enter q to quit."
 
-def welcome
-    return "Welcome to BattleShip"
-end
-
-def user_input(input)
+    input = gets.chomp
 
     if input == "p"
       "I have laid out my ships on the grid.\n You now need to lay out your two ships. \n The Cruiser is two units long and the Submarine is three units long."
     elsif input == "q"
       "Leaving BattleShip"
     else
-      "Invalid input."
+      "Invalid response." #how can i prompt the user back to lines 4/5 of code.
     end
+  end
+
+def initialize
 end
+
 end
