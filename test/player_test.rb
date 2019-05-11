45163d980c09e036746c39763edcee42c1c48442
Tay <tayd93@hotmail.com>
2019-05-11 13:18:15 -0600

    Updates PlayerTest with tests for existance/attributes/create_ships

--- test/player_test.rb
+++ test/player_test.rb
@@ -4,21 +4,32 @@ require 'minitest/pride'
 require './lib/ship'
 require './lib/cell'
 require './lib/board'
+require './lib/player'
 require 'pry'
 
 class PlayerTest < Minitest::Test
 
-  def setup
-    @player_1 = Player.new() #i want to initialize each player with a board object
-    @player_2 = Player.new()
-  end
+  # def setup
+  #   @player_1 = Player.new #i want to initialize each player with a board object
+  #   @player_2 = Player.new
+  # end
 
   def test_players_exist
-    assert_instance_of Player, @player_1
-    assert_instance_of Player, @player_2
+    player_1 = Player.new
+    player_2 = Player.new
+
+    assert_instance_of Player, player_1
+    assert_instance_of Player, player_2
   end
 
   def test_player_attributes
-    assert_equal
+    skip
+    assert_equal "#{name}", player.name
+    assert_equal @board, player.board
+  end
+
+  def test_create_ships
+    skip
+    assert_equal [cruiser, sub], player.create_board
   end
 end
