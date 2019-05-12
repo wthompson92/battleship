61e06ed5b142aec80e6b305d01cd94e9d0d961b7
Tay <tayd93@hotmail.com>
2019-05-12 12:02:38 -0600

    Begins Turn class/test skeleton

---
+++ lib/turn.rb
@@ -0,0 +1,25 @@
+class Turn 
+  attr_reader
+
+  def initialize
+
+  end
+
+  def start
+
+
+  end
+
+  def firing_queue
+    firing_queue = []
+      if @human == false
+        #generating number to be passed inside the method
+          @player.board.row.each do |letter|
+            @player.board.column.each do |number|
+              firing_queue << [letter, number]
+            end
+          end
+          firing_queue.shuffle!
+      end
+  end
+end
