591a605a0beb4d1d57e5ce2825214956351bc6d6
username <wnt925@gmail.com>
2019-05-13 23:34:21 -0600

    fix typo

--- lib/turn.rb
+++ lib/turn.rb
@@ -13,18 +13,18 @@ class Turn
     print "\n#{"=" * 3}PLAYER BOARD#{"=" * 3}\n #{@player_board.render}"
   end
 
-  def fire
+    def fire
     #change to or when computer places ships
-   until @player_board.all_sunk? == true && @computer_board.all_sunk? == true do
+    until @player_board.all_sunk? == true && @computer_board.all_sunk? == true do
       puts @message.take_shot_method
       coordinate = gets.chomp.to_s.upcase
-        if @computer_board.valid_coordinate?(coordinate) == false
-          puts "\n#{@message.invalid_shot_message}\n"
-        else
-      @computer_board.cells[coordinate].fire_upon
-      puts "\n#{"=" * 3}COMPUTER BOARD#{"=" * 3}\n #{@computer_board.render}"
-      puts "\n#{"=" * 3}PLAYER BOARD#{"=" * 3}\n #{@player_board.render(true)}"
+      if @computer_board.valid_coordinate?(coordinate) == false
+        puts "\n#{@message.invalid_shot_message}\n"
+      else
+        @computer_board.cells[coordinate].fire_upon
+        puts "\n#{"=" * 3}COMPUTER BOARD#{"=" * 3}\n #{@computer_board.render}"
+        puts "\n#{"=" * 3}PLAYER BOARD#{"=" * 3}\n #{@player_board.render(true)}"
+      end
     end
-  end
   end
 end
